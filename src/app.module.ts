f0a5ecf0788d8994ee5bb2da9e3af695e824583d
Manyonge <arthurmanyonge22@gmail.com>
2023-07-25 11:17:30 -0700

    in the process of implementing payment process

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentsController } from './payments/payments.controller';
+import { PaymentsModule } from './payments/payments.module';
+import { PaymentsService } from './payments/payments.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), PaymentsModule, HttpModule],
+  controllers: [AppController, PaymentsController],
+  providers: [AppService, PaymentsService],
 })
 export class AppModule {}
