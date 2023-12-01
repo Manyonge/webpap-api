c0d1a20c5f0916992f3c1611e113a94c8b82ce12
manyonge <arthurmanyonge22@gmail.com>
2023-12-01 23:24:14 +0300

    enabled cors for the gateway after 3 hours of debugging

--- src/payments/payments.gateway.ts
+++ src/payments/payments.gateway.ts
@@ -1,12 +1,32 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { OnModuleInit } from '@nestjs/common';
 
-@WebSocketGateway()
-export class PaymentsGateway {
+@WebSocketGateway({ cors: true })
+export class PaymentsGateway implements OnModuleInit {
   @WebSocketServer()
   server: Server;
 
+  onModuleInit(): any {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('connected');
+    });
+  }
+
   sendMessageToClients(event: string, data: any) {
     this.server.emit(event, data);
+  }
+
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: string): string {
+    console.log(data);
+    this.sendMessageToClients('onMessage', data);
+    return data;
   }
 }
